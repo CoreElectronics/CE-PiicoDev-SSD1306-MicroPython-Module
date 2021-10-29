681b5257a760eceb2068ced9ff4633dc63056674
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-10-29 13:31:58 +1100

    Update main.py - feature demo

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# PiicoDev SSD1306 minimal example code
-# This program writes to PiicoDev SSD1306 OLED display driver
+# PiicoDev SSD1306 demo code
+# Show off some features of the PiicoDev OLED driver
 
 import math
-from random import uniform
 from PiicoDev_SSD1306 import *
 from PiicoDev_Unified import sleep_ms
 
@@ -11,29 +10,41 @@ display = create_PiicoDev_SSD1306()
 WIDTH=127 # Dimensions of the OLED (px)
 HEIGHT=63
 
-print('Black')
-display.fill(0)
-display.show()
-sleep_ms(1000)
-# 
-print('White')
-display.fill(1)
-display.show()
-sleep_ms(1000)
-
-print('Rectangle')
-y = 30
-width = 20
-height = 35
-for x in range(0, 129):
-    y = y - 0.3
+# Text and numbers
+for counter in range(0,101):
     display.fill(0)
-    display.fill_rect(x, round(y), width, height, 1)
+    display.text("PiicoDev",30,20, 1)
+    display.text(str(counter),50,35, 1)
     display.show()
-    # no sleep necessary, the frame update time is enough
+sleep_ms(500)
 
 
-# Bouncy Square
+# Bargraphs
+thick = 15 # thickness of the bar
+for val in range(WIDTH+1):
+    display.fill(0)
+    display.text("Bargraphs", 20, 10, 1)
+    display.fill_rect(0, HEIGHT-thick, val, thick, 1) # Filled bar graph
+    display.rect(0, int(HEIGHT-2*thick - 5), int(val/2), thick, 1) # no-fill
+    display.show()
+sleep_ms(500)
+
+
+# Plots
+graphSin = display.graph2D()
+graphCos = display.graph2D()
+for x in range(128):
+    s = int(math.sin(x/10.0)*HEIGHT+HEIGHT+30)
+    c = int(math.cos(x/10.0)*HEIGHT+HEIGHT+30)
+    display.fill(0)
+    display.text("Plots", 50, 10, 1)
+    display.updateGraph2D(graphSin,s)
+    display.updateGraph2D(graphCos,c)
+    display.show()
+sleep_ms(1000)
+
+
+# Bouncy Square animation
 square = 15   # square edge length (px)
 x = WIDTH/2   # starting position
 y = HEIGHT/2  # starting y position
