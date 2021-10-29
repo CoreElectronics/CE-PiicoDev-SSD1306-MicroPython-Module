d67b08af66f5e754c20da505dec2aec3849c23c5
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-10-29 15:38:58 +1100

    update example

--- example/graph.py
+++ example/graph.py
@@ -6,8 +6,8 @@ graph1 = display.graph2D(minValue=-1, maxValue=1) # create two graph2D objects
 graph2 = display.graph2D(minValue=-4, maxValue=4)
 
 for x in range(WIDTH):
-    y = sin(2*pi*x/WIDTH) # a sinewave of amplitude 1, and wavelength equal to the screen width
-    z = (2*cos(6*pi*x/WIDTH)) # a cosinewave with amplitude 3, and wavelength one-third the screen width
+    y = sin(2*pi*x/WIDTH) # sine wave with amplitude 1, and wavelength equal to the screen width
+    z = 2*cos(6*pi*x/WIDTH) # cosine wave with amplitude 2, and wavelength one-third the screen width
     
     display.fill(0)
     display.updateGraph2D(graph1, y)
